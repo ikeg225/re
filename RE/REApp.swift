a6a976eec109f7120295fa5d8db03d149861ab44
Ethan Ikegami <ikeg225@berkeley.edu>
2022-06-01 23:24:21 -0700

    Initial Commit

---
+++ RE/REApp.swift
@@ -0,0 +1,17 @@
+//
+//  REApp.swift
+//  RE
+//
+//  Created by Ethan Ikegami on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct REApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
